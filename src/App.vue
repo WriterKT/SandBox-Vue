8c35fd658ee9a51ac08925d083ae77e98bf677f5
KurachiTK <xxxxx@gmail.com>
2023-09-02 15:28:48 +0900

    カスタムコンポーネントとカスタムイベントについて勉強

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <!--ChildCompo1のtemplateを呼び出す？-->
+        <c-comp1 @custom-event="handleCustomEvent"></c-comp1>
+        <p>{{ eventData }}</p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChildCompo1 from './components/ChildCompo1.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'c-comp1' : ChildCompo1
+  },
+  data() {
+    return {
+      eventData: null
+    };
+  },
+  methods: {
+    handleCustomEvent(data) {
+      this.eventData = data;
+    }
   }
+
 }
 </script>
 
---
+++ src/components/ChildCompo1.vue
@@ -0,0 +1,43 @@
+<template>
+    <ul>
+      <button @click="emitEvent">イベントを発火</button>
+      <button @click="emitEvent">文言を変える</button>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'ChileCompo1',
+  data() {
+    return {
+      eventData: 'ChildCompo1で定義した文言'
+    };
+  },
+  methods: {
+    emitEvent() {
+      this.$emit('custom-event', this.eventData); // 'custom-event'というカスタムイベントを発火
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  font-size: 10px;
+}
+</style>
